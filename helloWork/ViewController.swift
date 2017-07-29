83d8b8498e97d5a23d9ea34195f542119f406012
Anna Watson <annawatsonfly@gmail.com>
2017-07-29 23:22:04 +1000

    Initial Commit

---
+++ helloWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloWork
+//
+//  Created by Anna Denton on 29/7/17.
+//  Copyright © 2017 AnnaDev7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
